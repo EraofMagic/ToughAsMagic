fbdd47644fa25430a5ab4a511ae0450d759aa2a5
robdu <Lucien.zeiske@gmail.com>
2024-03-25 20:05:48 +0100

    Reworked WarmUp component

--- src/main/java/com/robdu/toughasmagic/spells/ComponentWarmUp.java
+++ src/main/java/com/robdu/toughasmagic/spells/ComponentWarmUp.java
@@ -2,34 +2,37 @@ package com.robdu.toughasmagic.spells;
 
 import com.mna.api.affinity.Affinity;
 import com.mna.api.spells.ComponentApplicationResult;
+import com.mna.api.spells.attributes.Attribute;
 import com.mna.api.spells.attributes.AttributeValuePair;
 import com.mna.api.spells.base.IModifiedSpellPart;
+import com.mna.api.spells.base.ISpellDefinition;
 import com.mna.api.spells.parts.SpellEffect;
 import com.mna.api.spells.targeting.SpellContext;
 import com.mna.api.spells.targeting.SpellSource;
 import com.mna.api.spells.targeting.SpellTarget;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 import toughasnails.api.temperature.ITemperature;
 import toughasnails.api.temperature.TemperatureHelper;
-import toughasnails.api.temperature.TemperatureLevel;
 
 public class ComponentWarmUp extends SpellEffect {
     public ComponentWarmUp(ResourceLocation registryName, ResourceLocation guiIcon, AttributeValuePair... attributeValuePairs) {
-        super(registryName, guiIcon, attributeValuePairs);
+        super(registryName, guiIcon, new AttributeValuePair(Attribute.MAGNITUDE, 1.0F, 1.0F, 5.0F, 1.0F, 5.0F));
     }
 
     @Override
     public ComponentApplicationResult ApplyEffect(SpellSource spellSource, SpellTarget spellTarget, IModifiedSpellPart<SpellEffect> iModifiedSpellPart, SpellContext spellContext) {
-        if(spellTarget.getEntity() instanceof Player player) {
+        if (spellTarget.getEntity() instanceof Player player) {
             ITemperature playertemp = TemperatureHelper.getTemperatureData(player);
-            playertemp.setLevel(TemperatureLevel.WARM);
+            playertemp.setLevel(playertemp.getLevel().increment((int)iModifiedSpellPart.getValue(Attribute.MAGNITUDE)));
             return ComponentApplicationResult.SUCCESS;
-
-        } else {
-            return ComponentApplicationResult.FAIL;
+             }
+         return ComponentApplicationResult.FAIL;
         }
-    }
 
     @Override
     public Affinity getAffinity() {
