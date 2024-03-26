1bcb80a0b510b4d195491bc5a18461c12a633de6
robdu <Lucien.zeiske@gmail.com>
2024-03-26 13:57:31 +0100

    Added Particles with math to components

--- src/main/java/com/robdu/toughasmagic/spells/ComponentCooldown.java
+++ src/main/java/com/robdu/toughasmagic/spells/ComponentCooldown.java
@@ -1,21 +1,33 @@
 package com.robdu.toughasmagic.spells;
 
 import com.mna.api.affinity.Affinity;
+import com.mna.api.particles.MAParticleType;
+import com.mna.api.particles.ParticleInit;
 import com.mna.api.spells.ComponentApplicationResult;
 import com.mna.api.spells.attributes.Attribute;
 import com.mna.api.spells.attributes.AttributeValuePair;
 import com.mna.api.spells.base.IModifiedSpellPart;
+import com.mna.api.spells.base.ISpellDefinition;
 import com.mna.api.spells.parts.SpellEffect;
 import com.mna.api.spells.targeting.SpellContext;
 import com.mna.api.spells.targeting.SpellSource;
 import com.mna.api.spells.targeting.SpellTarget;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 import toughasnails.api.temperature.ITemperature;
 import toughasnails.api.temperature.TemperatureHelper;
 import toughasnails.api.temperature.TemperatureLevel;
 
 public class ComponentCooldown extends SpellEffect {
+
+    private static double x = 0;
+    private static double y = 0;
+    private static double z = 0;
+
     public ComponentCooldown(ResourceLocation registryName, ResourceLocation guiIcon, AttributeValuePair... attributeValuePairs) {
         super(registryName, guiIcon, new AttributeValuePair(Attribute.MAGNITUDE, 1.0F, 1.0F, 5.0F, 1.0F, 5.0F));
     }
@@ -23,7 +35,13 @@ public class ComponentCooldown extends SpellEffect {
     @Override
     public ComponentApplicationResult ApplyEffect(SpellSource spellSource, SpellTarget spellTarget, IModifiedSpellPart<SpellEffect> iModifiedSpellPart, SpellContext spellContext) {
         if (spellTarget.getEntity() instanceof Player player) {
+
+            x = player.getX();
+            y = player.getY();
+            z = player.getZ();
+
             ITemperature playertemp = TemperatureHelper.getTemperatureData(player);
+
             playertemp.setLevel(playertemp.getLevel().decrement((int)iModifiedSpellPart.getValue(Attribute.MAGNITUDE)));
             return ComponentApplicationResult.SUCCESS;
         }
@@ -48,5 +66,21 @@ public class ComponentCooldown extends SpellEffect {
     @Override
     public boolean canBeOnRandomStaff() {
         return super.canBeOnRandomStaff();
+    }
+
+    @Override
+    public void SpawnParticles(Level world, Vec3 impact_position, Vec3 normal, int age, @Nullable LivingEntity caster, @Nullable ISpellDefinition recipe) {
+        for(int i = 0; i < 360; i++){
+            if(i % 5 == 0 && age <= 5) {
+                world.addParticle(recipe.colorParticle(new MAParticleType(ParticleInit.FROST.get()),caster),
+                        x+-Math.sin(i*Math.PI/180)*1,
+                        y + (float)i / 100,
+                        z+Math.cos(i*Math.PI/180)*1,
+                        0.0d,
+                        0.1d,
+                        0.0d);
+            }
+        }
+
     }
 }
--- src/main/java/com/robdu/toughasmagic/spells/ComponentWarmUp.java
+++ src/main/java/com/robdu/toughasmagic/spells/ComponentWarmUp.java
@@ -1,20 +1,32 @@
 package com.robdu.toughasmagic.spells;
 
 import com.mna.api.affinity.Affinity;
+import com.mna.api.particles.MAParticleType;
+import com.mna.api.particles.ParticleInit;
 import com.mna.api.spells.ComponentApplicationResult;
 import com.mna.api.spells.attributes.Attribute;
 import com.mna.api.spells.attributes.AttributeValuePair;
 import com.mna.api.spells.base.IModifiedSpellPart;
+import com.mna.api.spells.base.ISpellDefinition;
 import com.mna.api.spells.parts.SpellEffect;
 import com.mna.api.spells.targeting.SpellContext;
 import com.mna.api.spells.targeting.SpellSource;
 import com.mna.api.spells.targeting.SpellTarget;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 import toughasnails.api.temperature.ITemperature;
 import toughasnails.api.temperature.TemperatureHelper;
 
 public class ComponentWarmUp extends SpellEffect {
+
+    private static double x = 0;
+    private static double y = 0;
+    private static double z = 0;
+
     public ComponentWarmUp(ResourceLocation registryName, ResourceLocation guiIcon, AttributeValuePair... attributeValuePairs) {
         super(registryName, guiIcon, new AttributeValuePair(Attribute.MAGNITUDE, 1.0F, 1.0F, 5.0F, 1.0F, 5.0F));
     }
@@ -22,6 +34,11 @@ public class ComponentWarmUp extends SpellEffect {
     @Override
     public ComponentApplicationResult ApplyEffect(SpellSource spellSource, SpellTarget spellTarget, IModifiedSpellPart<SpellEffect> iModifiedSpellPart, SpellContext spellContext) {
         if (spellTarget.getEntity() instanceof Player player) {
+
+            x = player.getX();
+            y = player.getY();
+            z = player.getZ();
+
             ITemperature playertemp = TemperatureHelper.getTemperatureData(player);
             playertemp.setLevel(playertemp.getLevel().increment((int)iModifiedSpellPart.getValue(Attribute.MAGNITUDE)));
             return ComponentApplicationResult.SUCCESS;
@@ -47,5 +64,21 @@ public class ComponentWarmUp extends SpellEffect {
     @Override
     public boolean canBeOnRandomStaff() {
         return super.canBeOnRandomStaff();
+    }
+
+    @Override
+    public void SpawnParticles(Level world, Vec3 impact_position, Vec3 normal, int age, @Nullable LivingEntity caster, @Nullable ISpellDefinition recipe) {
+        for(int i = 0; i < 360; i++){
+            if(i % 2.5 == 0 && age <= 5) {
+                world.addParticle(recipe.colorParticle(new MAParticleType(ParticleInit.FLAME.get()),caster),
+                        x+-Math.sin(i*Math.PI/180)*1,
+                        y + (float)i / 100,
+                        z+Math.cos(i*Math.PI/180)*1,
+                        0.0d,
+                        0.1d,
+                        0.0d);
+            }
+        }
+
     }
 }
