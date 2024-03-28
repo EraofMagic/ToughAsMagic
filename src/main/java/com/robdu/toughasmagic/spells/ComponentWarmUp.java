c13dcce5c5dcab3da34301fabb61709e831b857e
ToCraft <piminecraft492@gmail.com>
2024-03-28 10:28:56 +0100

    fix particles

--- src/main/java/com/robdu/toughasmagic/spells/ComponentWarmUp.java
+++ src/main/java/com/robdu/toughasmagic/spells/ComponentWarmUp.java
@@ -22,12 +22,7 @@ import toughasnails.api.temperature.ITemperature;
 import toughasnails.api.temperature.TemperatureHelper;
 
 public class ComponentWarmUp extends SpellEffect {
-
-    private static double x = 0;
-    private static double y = 0;
-    private static double z = 0;
-
-    public ComponentWarmUp(ResourceLocation registryName, ResourceLocation guiIcon, AttributeValuePair... attributeValuePairs) {
+    public ComponentWarmUp(ResourceLocation registryName, ResourceLocation guiIcon) {
         super(registryName, guiIcon, new AttributeValuePair(Attribute.MAGNITUDE, 1.0F, 1.0F, 5.0F, 1.0F, 5.0F));
     }
 
@@ -35,16 +30,12 @@ public class ComponentWarmUp extends SpellEffect {
     public ComponentApplicationResult ApplyEffect(SpellSource spellSource, SpellTarget spellTarget, IModifiedSpellPart<SpellEffect> iModifiedSpellPart, SpellContext spellContext) {
         if (spellTarget.getEntity() instanceof Player player) {
 
-            x = player.getX();
-            y = player.getY();
-            z = player.getZ();
-
             ITemperature playertemp = TemperatureHelper.getTemperatureData(player);
-            playertemp.setLevel(playertemp.getLevel().increment((int)iModifiedSpellPart.getValue(Attribute.MAGNITUDE)));
+            playertemp.setLevel(playertemp.getLevel().increment((int) iModifiedSpellPart.getValue(Attribute.MAGNITUDE)));
             return ComponentApplicationResult.SUCCESS;
-             }
-         return ComponentApplicationResult.FAIL;
         }
+        return ComponentApplicationResult.FAIL;
+    }
 
     @Override
     public Affinity getAffinity() {
@@ -62,21 +53,18 @@ public class ComponentWarmUp extends SpellEffect {
     }
 
     @Override
-    public boolean canBeOnRandomStaff() {
-        return super.canBeOnRandomStaff();
-    }
-
-    @Override
     public void SpawnParticles(Level world, Vec3 impact_position, Vec3 normal, int age, @Nullable LivingEntity caster, @Nullable ISpellDefinition recipe) {
-        for(int i = 0; i < 360; i++){
-            if(i % 2.5 == 0 && age <= 5) {
-                world.addParticle(recipe.colorParticle(new MAParticleType(ParticleInit.FLAME_ORBIT.get().setColor(100,0,0,100)),caster),
-                        x+-Math.sin(i*Math.PI/90)*1,
-                        y + (float)i / 100,
-                        z+Math.cos(i*Math.PI/90)*1,
-                        1.0d,
-                        0.1d,
-                        0.0d);
+        if (recipe != null && age <= 5) {
+            for (int i = 0; i <= 360; i++) {
+                if (i % 2.5 == 0) {
+                    world.addParticle(recipe.colorParticle(new MAParticleType(ParticleInit.FLAME_ORBIT.get().setColor(100, 0, 0, 100)), caster),
+                            impact_position.x - Math.sin(Math.toRadians(i)),
+                            impact_position.y + (double) i / 100,
+                            impact_position.z + Math.cos(Math.toRadians(i)),
+                            1.0d,
+                            0.1d,
+                            0.0d);
+                }
             }
         }
     }
